4c9c0f8ba1f2778387ff244af50b29890090b68e
Kelly Morning <kleemorning@gmail.com>
2021-02-09 17:54:53 -0500

    GetDomains trial

--- desec/api.go
+++ desec/api.go
@@ -1,6 +1,8 @@
 package desec
 
 import (
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -12,8 +14,25 @@ type API struct {
 	Token string
 }
 
-// TODO: Use correct response type to return
-func (a *API) Request(method, path string, body io.Reader) (*http.Response, error) {
+// ErrorResponse defines the error response format
+type ErrorResponse struct {
+	Detail string `json:"detail,omitempty"`
+}
+
+// DNSDomain defines the format of a Domain object
+type DNSDomain struct {
+	Created    string `json:"created,omitempty"`
+	Published  string `json:"published,omitempty"`
+	Name       string `json:"name,omitempty"`
+	MinimumTTL int    `json:"minimum_ttl,omitempty"`
+	Touched    string `json:"touched,omitempty"`
+}
+
+// DNSDomains is an array of Domain objects
+type DNSDomains []DNSDomain
+
+// Request builds the raw request
+func (a *API) request(method, path string, body io.Reader) (*http.Response, error) {
 	if path[0] != '/' {
 		path = "/" + path
 	}
@@ -27,6 +46,7 @@ func (a *API) Request(method, path string, body io.Reader) (*http.Response, erro
 		return nil, err
 	}
 	req.Header.Set("Authorization", "Token "+a.Token)
+	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Content-Type", "application/json")
 
 	// TODO: add k8s logging
@@ -35,5 +55,37 @@ func (a *API) Request(method, path string, body io.Reader) (*http.Response, erro
 	if err != nil {
 		return nil, err
 	}
+
 	return rawResp, nil
 }
+
+// GetDomains gets an array of domains
+func (a *API) GetDomains() (DNSDomains, error) {
+	method, path := "GET", "domains/"
+	resp, err := a.request(method, path, nil)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode == http.StatusOK {
+		var domains DNSDomains
+		if err := json.NewDecoder(resp.Body).Decode(&domains); err != nil {
+			return nil, fmt.Errorf("%s %s response parsing error: %v", method, path, err)
+		}
+		return domains, nil
+	}
+	var errResp ErrorResponse
+	if err := json.NewDecoder(resp.Body).Decode(&errResp); err != nil {
+		return nil, fmt.Errorf("%s %s unknown error occured", method, path)
+	}
+	return nil, fmt.Errorf("%s %s error: %s", method, path, errResp.Detail)
+}
+
+/*
+var resp APIResponse
+	if err := json.NewDecoder(rawResp.Body).Decode(&resp); err != nil {
+		return nil, fmt.Errorf("%s %s response parsing error: %v", method, path, err)
+	}
+
+	// rawJSON, _ := json.Marshal(resp)
+	// TODO: klog.V(8).Infof("Response status: `%s` json response: `%v`", rawResp.Status, string(rawJSON))
+*/
