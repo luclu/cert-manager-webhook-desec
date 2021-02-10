dc6dd8ebd4e109eba5192ef4804e30ad820651a7
Kelly Morning <kleemorning@gmail.com>
2021-02-10 11:08:27 -0500

    implement RRSets

--- desec/api.go
+++ desec/api.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -29,8 +30,21 @@ type DNSDomain struct {
 	Touched    string `json:"touched,omitempty"`
 }
 
-// DNSDomains is an array of Domain objects
+// DNSDomains is a slice of Domain objects
 type DNSDomains []DNSDomain
+
+// RRSet defines the format of a Resource Record Set object
+type RRSet struct {
+	Domain  string   `json:"domain,omitempty"`
+	SubName string   `json:"subname,omitempty"`
+	Name    string   `json:"name,omitempty"`
+	Type    string   `json:"type,omitempty"`
+	Records []string `json:"records,omitempty"`
+	TTL     int      `json:"ttl,omitempty"`
+}
+
+// RRSets is a slice of Resource Record Set objects
+type RRSets []RRSet
 
 // Request builds the raw request
 func (a *API) request(method, path string, body io.Reader, target interface{}) error {
@@ -72,8 +86,8 @@ func (a *API) request(method, path string, body io.Reader, target interface{}) e
 	return nil
 }
 
-// GetDomains - returns all dns domains managed by deSEC
-func (a *API) GetDomains() (DNSDomains, error) {
+// GetDNSDomains - returns all dns domains managed by deSEC
+func (a *API) getDNSDomains() (DNSDomains, error) {
 	method, path := "GET", "domains/"
 	domains := new(DNSDomains)
 	err := a.request(method, path, nil, domains)
@@ -83,12 +97,35 @@ func (a *API) GetDomains() (DNSDomains, error) {
 	return *domains, nil
 }
 
-/*
-var resp APIResponse
-	if err := json.NewDecoder(rawResp.Body).Decode(&resp); err != nil {
-		return nil, fmt.Errorf("%s %s response parsing error: %v", method, path, err)
+// GetDNSDomain - get the dns domain that matches the target fqdn
+func (a *API) GetDNSDomain(fqdn string) (*DNSDomain, error) {
+	domains, err := a.getDNSDomains()
+	if err != nil {
+		return nil, err
 	}
+	for _, v := range domains {
+		if strings.HasSuffix(fqdn, v.Name) {
+			return &v, nil
+		}
+	}
+	return nil, fmt.Errorf("domain not found")
+}
 
-	// rawJSON, _ := json.Marshal(resp)
-	// TODO: klog.V(8).Infof("Response status: `%s` json response: `%v`", rawResp.Status, string(rawJSON))
-*/
+// GetRRSets returns all TXT records for the subdomain portion of fqdn
+func (a *API) GetRRSets(fqdn string) (RRSets, error) {
+	dnsDomain, err := a.GetDNSDomain(fqdn)
+	if err != nil {
+		return nil, err
+	}
+	domain := dnsDomain.Name
+	subname := fqdn[:len(fqdn)-len(domain)-1]
+	method := "GET"
+	path := "domains/" + domain + "/rrsets/?subname=" + subname + "&type=TXT"
+
+	rrsets := new(RRSets)
+	err = a.request(method, path, nil, rrsets)
+	if err != nil {
+		return nil, err
+	}
+	return *rrsets, nil
+}
